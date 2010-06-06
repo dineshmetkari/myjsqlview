ee6563aa3cd5dd68a8458168af73512babb295b5
danap <danap@dandymadeproductions.com>
2010-06-06 01:06:41 +0000

    Removed Class Methods getTabIcon(), getMenuBar(), & getToolBar(). Removed Delay in InitPlugin() Class Method. Parameterized Argument tableNames in Class Method setDBTables() to Bring Code Into Compliance With Java 5.0 API.

--- myjsqlview/docs/Plugins/PluginBasics/Functional/PluginModule.java
+++ myjsqlview/docs/Plugins/PluginBasics/Functional/PluginModule.java
@@ -32,6 +32,10 @@
 //=================================================================
 // Version 1.0 Original TableRecordCount MyJSQLView_PluginModule.
 //         1.1 Formatted and Added Delay in Class Method initPlugin().
+//         1.2 Removed Class Methods getTabIcon(), getMenuBar(), & getToolBar().
+//             Removed Delay in initPlugin() Class Method. Parameterized Argument
+//             tableNames in Class Method setDBTables() to Bring Code Into
+//             Compliance With Java 5.0 API.
 //
 //-----------------------------------------------------------------
 //                 danap@dandymadeproductions.com
@@ -40,9 +44,6 @@
 package com.dandymadeproductions.tablerecordcount;
 
 import javax.swing.JPanel;
-import javax.swing.JMenuBar;
-import javax.swing.JToolBar;
-import javax.swing.ImageIcon;
 import java.util.Vector;
 
 import com.dandymadeproductions.myjsqlview.MyJSQLView_Frame;
@@ -54,7 +55,7 @@ import com.dandymadeproductions.myjsqlview.MyJSQLView_PluginModule;
  * module into the MyJSQLView application.
  * 
  * @author Dana M. Proctor
- * @version 1.1 05/29/2010
+ * @version 1.2 06/05/2010
  */
 
 public class PluginModule extends MyJSQLView_PluginModule
@@ -78,18 +79,6 @@ public class PluginModule extends MyJSQLView_PluginModule
    public void initPlugin(MyJSQLView_Frame parentFrame)
    {
       tableRecordCountPanel = new TableRecordCountPanel(MyJSQLView_Access.getTableNames());
-      
-      // Need a delay because of a bug with
-      // 3.17 for a simple plugin like this.
-      // Fixed already for next version.
-      try
-      {
-         Thread.sleep(8000);
-      }
-      catch (InterruptedException e)
-      {
-         System.out.println("Process Interrupted.");
-      }
    }
 
    //==============================================================
@@ -100,38 +89,6 @@ public class PluginModule extends MyJSQLView_PluginModule
    public String getName()
    {
       return "Table Record Counts";
-   }
-
-   //==============================================================
-   // Class method to allow the collection of a image icon that
-   // will be used as an identifier in the MyJSQLView tab structure.
-   // NOTE: The tab icon should be no larger than 12 x 12.
-   //==============================================================
-
-   public ImageIcon getTabIcon()
-   {
-      return null;
-   }
-
-   //==============================================================
-   // Class method to meet the interface requirements of returning
-   // a JMenuBar that can be used to control various aspects of
-   // the modules functionality.
-   //==============================================================
-
-   public JMenuBar getMenuBar()
-   {
-      return null;
-   }
-
-   //==============================================================
-   // Class method to allow the collection of a JToolBar to be
-   // used with the plugin module.
-   //==============================================================
-
-   public JToolBar getToolBar()
-   {
-      return null;
    }
 
    //==============================================================
@@ -150,7 +107,7 @@ public class PluginModule extends MyJSQLView_PluginModule
    // able to set the database tables.
    //==============================================================
 
-   public void setDBTables(Vector tableNames)
+   public void setDBTables(Vector<String> tableNames)
    {
       tableRecordCountPanel.reloadPanel(tableNames);
       tableRecordCountPanel.repaint();
