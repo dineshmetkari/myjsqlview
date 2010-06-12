f071ba0f19c844829960182f9dec864b2c842dcd
danap <danap@dandymadeproductions.com>
2010-06-12 15:37:45 +0000

    Changes to Reduce the Resources Contained in the Jar.

--- myjsqlview/build.bat
+++ myjsqlview/build.bat
@@ -1,1 +1,1 @@
-jar cvfm MyJSQLView.jar myjsqlview.mf -C bin\ com docs\Manual docs\Release\*.html images\*.jpg images\*.png images\*.gif images\icons locale\*.properties sounds\*.wav *.txt myjsqlview.xml
+jar cvfm MyJSQLView.jar myjsqlview.mf -C bin\ com docs\Manual docs\Release\*.html
