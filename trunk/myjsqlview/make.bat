b2dbc9639eab66654e9682cf72ae63aa92a7ecd8
danap@dandymadeproductions.com <danap@dandymadeproductions.com@5ac04320-0ce5-11de-8db4-35e3754be71a>
2010-06-12 15:36:57 +0000

    Update to Reflect the Addition of iText Library Compiliation Needs.

--- trunk/myjsqlview/make.bat
+++ trunk/myjsqlview/make.bat
@@ -1,1 +1,1 @@
-javac -source 1.4 -d bin -verbose src\main\com\dandymadeproductions\myjsqlview\*.java src\misc\com\dandymadeproductions\myjsqlview\*.java src\panels\com\dandymadeproductions\myjsqlview\*.java src\utilities\com\dandymadeproductions\myjsqlview\*.java
+javac -source 1.5 -d bin -verbose -classpath lib/iText-5.0.2.jar src\main\com\dandymadeproductions\myjsqlview\*.java src\misc\com\dandymadeproductions\myjsqlview\*.java src\panels\com\dandymadeproductions\myjsqlview\*.java src\utilities\com\dandymadeproductions\myjsqlview\*.java
