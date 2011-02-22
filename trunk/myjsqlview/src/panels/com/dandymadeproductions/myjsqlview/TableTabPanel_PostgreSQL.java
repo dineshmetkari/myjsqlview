150dd1dca90d3f8df93786c477b1f464a42cf2bf
danap@dandymadeproductions.com <danap@dandymadeproductions.com@5ac04320-0ce5-11de-8db4-35e3754be71a>
2011-02-22 01:46:33 +0000

    Corrections in Method loadTable to Properly Build searchTextString for Date, DateTime, & Timestamp When No Specific Column is Selected.

--- trunk/myjsqlview/src/panels/com/dandymadeproductions/myjsqlview/TableTabPanel_PostgreSQL.java
+++ trunk/myjsqlview/src/panels/com/dandymadeproductions/myjsqlview/TableTabPanel_PostgreSQL.java
@@ -13,7 +13,7 @@
 //
 //==============================================================
 // Copyright (C) 2007-2011 Dana M. Proctor
-// Version 12.3 01/26/2011
+// Version 12.4 02/21/2011
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -287,6 +287,8 @@
 //        12.3 Changes to Class Methods getColumnNames(), loadTable(), viewSelectedItem(),
 //             & deleteSelectedItem() to Used Newly Redefined ConnectionManager to Display
 //             SQL Errors.
+//        12.4 Corrections in Method loadTable to Properly Build searchTextString For Date,
+//             DateTime, & Timestamp When No Specific Column is Selected.
 //             
 //-----------------------------------------------------------------
 //                  danap@dandymadeproductions.com
@@ -312,7 +314,7 @@ import java.util.Iterator;
  * the mechanism to page through the database table's data.
  * 
  * @author Dana M. Proctor
- * @version 12.3 01/26/2011
+ * @version 12.4 02/21/2011
  */
 
 public class TableTabPanel_PostgreSQL extends TableTabPanel //implements ActionListener
@@ -559,22 +561,38 @@ public class TableTabPanel_PostgreSQL extends TableTabPanel //implements ActionL
          searchQueryString.append("'1' LIKE '%'");
       else
       {
+         // No field specified so build search for all.
          if (columnSearchString == null)
          {
             String[] tableColumns;
             tableColumns = sqlTableFieldsString.split(",");
-
+            
             for (int i = 0; i < tableColumns.length; i++)
             {
+               columnName = tableColumns[i].replaceAll(identifierQuoteString, "");
+               columnType = columnTypeHashMap.get(parseColumnNameField(columnName.trim()));
+               
+               if (columnType.equals("DATE"))
+                  searchTextString = MyJSQLView_Utils.processDateFormatSearch(searchTextString);
+               else if (columnType.equals("TIMESTAMP") || columnType.equals("TIMESTAMPTZ"))
+               {
+                  if (searchTextString.indexOf(" ") != -1)
+                     searchTextString = MyJSQLView_Utils.processDateFormatSearch(
+                        searchTextString.substring(0, searchTextString.indexOf(" ")))
+                        + searchTextString.substring(searchTextString.indexOf(" "));
+                  else if (searchTextString.indexOf("-") != -1 || searchTextString.indexOf("/") != -1)
+                     searchTextString = MyJSQLView_Utils.processDateFormatSearch(searchTextString);
+               }
+               
                if (i < tableColumns.length - 1)
                   searchQueryString.append(tableColumns[i] + "::TEXT LIKE '%" + searchTextString + "%' OR");
                else
                   searchQueryString.append(tableColumns[i] + "::TEXT LIKE '%" + searchTextString + "%'");
             }
          }
+         // Field specified.
          else
          {
-            // Try and process Date/Datetime/TimeStamp Fields
             columnType = columnTypeHashMap.get(searchComboBox.getSelectedItem());
             
             if (columnType.equals("DATE"))
