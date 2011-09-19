84a253a3a198906cca818005c98f48498296e532
danap@dandymadeproductions.com <danap@dandymadeproductions.com@5ac04320-0ce5-11de-8db4-35e3754be71a>
2011-09-19 19:35:23 +0000

    Updated to Correctly Implement Requirements.

--- trunk/myjsqlview/test/msaccessTypes.sql
+++ trunk/myjsqlview/test/msaccessTypes.sql
@@ -4,36 +4,38 @@
 -- =============================================================
 -- Dana Proctor 
 -- Version 06/02/2011 Orignal Test Data Type Table.
+--         09/19/2011 Updated to Correctly Implement Requirements.
 --      
 -- danap@dandymadeproductions.com
 -- =============================================================
 
-DROP TABLE IF EXISTS msaccesstypes;
+-- Drop Table MUST be in separate file, import.
+-- DROP TABLE msaccesstypes;
 CREATE TABLE msaccesstypes (
 
 --  Table id and creation data entries.
 
-    data_type_id COUNTER PRIMARY KEY,
+	data_type_id COUNTER PRIMARY KEY,
 
 --  Character, text, and blob type fields.
 
-    binary_type BINARY DEFAULT NULL,
-    longbinary_type LONGBINARY DEFAULT NULL,
-    text_type TEXT DEFAULT NULL,
-    longText_type LONGTEXT DEFAULT NULL,
+    binary_type BINARY,
+    longbinary_type LONGBINARY,
+    varchar_type TEXT(50),
+    longText_type LONGTEXT,
 
 --  Numeric fields.
 
-    bit_type BIT DEFAULT NULL,
-    byte_type BYTE DEFAULT NULL,
-    short_type SHORT DEFAULT NULL,
-    long_type LONG DEFAULT NULL,
-    single_type SINGLE DEFAULT NULL,
-    double_type DOUBLE DEFAULT NULL,
-    currency_type CURRENCY DEFAULT NULL,
-    guid_type GUID DEFAULT NUll,
+    bit_type BIT,
+    byte_type BYTE,
+    short_type SHORT,
+    long_type LONG,
+    single_type SINGLE,
+    double_type DOUBLE,
+    currency_type CURRENCY,
+    guid_type GUID,
     
 --  Date and time fields.
    
-    dateTime_type DATETIME DEFAULT NULL
+    dateTime_type DATETIME
 );
