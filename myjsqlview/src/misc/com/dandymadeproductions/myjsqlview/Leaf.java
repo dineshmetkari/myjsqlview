0881ea94f0b1edba5778fbf147726e11d1f34442
danap <danap@dandymadeproductions.com>
2011-12-31 20:44:01 +0000

    Copyright Update.

--- myjsqlview/src/misc/com/dandymadeproductions/myjsqlview/Leaf.java
+++ myjsqlview/src/misc/com/dandymadeproductions/myjsqlview/Leaf.java
@@ -9,8 +9,8 @@
 //                    << Leaf.java >>
 //
 //=================================================================
-// Copyright (C) 2007-2011 Dana M. Proctor
-// Version 1.7 01/27/2011
+// Copyright (C) 2007-2012 Dana M. Proctor
+// Version 1.8 01/01/2012
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -40,6 +40,7 @@
 //         1.5 Changed Package to Reflect Dandy Made Productions Code.
 //         1.6 Organized Imports.
 //         1.7 Copyright Update.
+//         1.8 Copyright Update.
 //        
 //-----------------------------------------------------------------
 //               danap@dandymadeproductions.com
@@ -58,7 +59,7 @@ import java.awt.Rectangle;
  * fall.
  * 
  * @author Dana M. Proctor
- * @version 1.7 01/27/2010
+ * @version 1.8 01/01/2012
  */
 
 class Leaf extends Sprite
--- myjsqlview/src/misc/com/dandymadeproductions/myjsqlview/SnowFlake.java
+++ myjsqlview/src/misc/com/dandymadeproductions/myjsqlview/SnowFlake.java
@@ -9,8 +9,8 @@
 //                    << SnowFlake.java >>
 //
 //=================================================================
-// Copyright (C) 2007-2011 Dana M. Proctor
-// Version 1.8 01/27/2011
+// Copyright (C) 2007-2012 Dana M. Proctor
+// Version 1.9 01/01/2012
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -41,6 +41,7 @@
 //         1.6 Changed Package to Reflect Dandy Made Productions Code.
 //         1.7 Organized Imports.
 //         1.8 Copyright Update.
+//         1.9 Copyright Update.
 //        
 //-----------------------------------------------------------------
 //               danap@dandymadeproductions.com
@@ -59,7 +60,7 @@ import java.awt.Rectangle;
  * winter.
  * 
  * @author Dana M. Proctor
- * @version 1.8 01/27/2011
+ * @version 1.9 01/01/2012
  */
 
 class SnowFlake extends Sprite
