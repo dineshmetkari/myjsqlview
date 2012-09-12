b2ff7b3b6cd3b7d054b3be90958abeb46a9b2314
danap@dandymadeproductions.com <danap@dandymadeproductions.com@5ac04320-0ce5-11de-8db4-35e3754be71a>
2012-09-12 23:05:39 +0000

    Changed Package Name to com.dandymadeproductions/myjsqlview.gui.panels.

---
+++ trunk/myjsqlview/src/com/dandymadeproductions/myjsqlview/gui/panels/PreferencesPanelInterface.java
@@ -0,0 +1,74 @@
+//=================================================================
+//            MyJSQLView PreferencesPanelInterface
+//=================================================================
+//    This class defines the methods that are required by all
+// PreferencesPanels in order to properly function within the 
+// MyJSQLView application with other classes. PreferencesPanels
+// Should All be Animatied Scenes
+//
+//            << PreferencesPanelInterface.java >>
+//
+//=================================================================
+// Copyright (C) 2007-2012 Dana M. Proctor
+// Version 1.7 09/11/2012
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version
+// 2 of the License, or (at your option) any later version. This
+// program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+// the GNU General Public License for more details. You should
+// have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// (http://opensource.org)
+//
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+// Version 1.0 Initial MyJSQLView PreferencesPanelInterface Class.
+//         1.1 Header Update.
+//         1.2 MyJSQLView Project Common Source Code Formatting.
+//         1.3 Header Format Changes/Update.
+//         1.4 Changed Package to Reflect Dandy Made Productions Code.
+//         1.5 Copyright Update.
+//         1.6 Copyright Update.
+//         1.7 Changed Package Name to com.dandymadeproductions.myjsqlview.gui.panels.
+//
+//-----------------------------------------------------------------
+//                  danap@dandymadeproductions.com
+//=================================================================
+
+package com.dandymadeproductions.myjsqlview.gui.panels;
+
+/**
+ *    The PreferencesPanelInterface class defines the methods that
+ * are required by all PreferencesPanels in order to properly
+ * function within the MyJSQLView application with other classes.
+ * PreferencesPanels Should All be Animatied Scenes.
+ * 
+ * @author Dana M. Proctor
+ * @version 1.7 09/11/2012
+ */
+
+interface PreferencesPanelInterface
+{
+   //==============================================================
+   // Class Method to start and stop the thread.
+   //==============================================================
+
+   void setThreadAction(boolean action);
+
+   //==============================================================
+   // Class Method to let the thread run() method naturally
+   // finish.
+   //==============================================================
+
+   void suspendPanel(boolean action);
+
+}
