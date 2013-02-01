a35168a52b78acd8a6fb71eaa0e28db63dc68e52
danap <danap@dandymadeproductions.com>
2013-02-01 18:20:28 +0000

    Updated Copyright Information.

--- myjsqlview/src/com/dandymadeproductions/myjsqlview/gui/sprites/SnowFlake.java
+++ myjsqlview/src/com/dandymadeproductions/myjsqlview/gui/sprites/SnowFlake.java
@@ -9,7 +9,7 @@
 //                    << SnowFlake.java >>
 //
 //=================================================================
-// Copyright (C) 2007-2012 Dana M. Proctor
+// Copyright (C) 2005-2013 Dana M. Proctor
 // Version 2.0 09/11/2012
 //
 // This program is free software; you can redistribute it and/or
