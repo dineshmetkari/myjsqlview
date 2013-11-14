2f7df208da18070df8cfd98dcc725cf3ddc332e1
danap <danap@dandymadeproductions.com>
2013-11-14 05:28:27 +0000

    Class Instance myJSQLView_Version Update for Release 3.47.

--- myjsqlview/src/com/dandymadeproductions/myjsqlview/MyJSQLView.java
+++ myjsqlview/src/com/dandymadeproductions/myjsqlview/MyJSQLView.java
@@ -15,7 +15,7 @@
 //
 //=================================================================
 // Copyright (C) 2005-2013 Dana M. Proctor
-// Version 3.46 08/12/2013
+// Version 3.47 11/13/2013
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -226,6 +226,8 @@
 //                         for the Application.
 //         3.46 08/12/2013 Class Instance myJSQLView_Version Update for Release
 //                         3.46.
+//         3.47 11/13/2013 Class Instance myJSQLView_Version Update for Release
+//                         3.47
 //         
 //-----------------------------------------------------------------
 //                 danap@dandymadeproductions.com
@@ -268,7 +270,7 @@ import com.dandymadeproductions.myjsqlview.utilities.MyJSQLView_Utils;
  * Arguments -debug, -lang='locale'
  * 
  * @author Dana M. Proctor
- * @version 3.46 08/12/2013
+ * @version 3.47 11/13/2013
  */
 
 public class MyJSQLView implements ActionListener
@@ -290,7 +292,7 @@ public class MyJSQLView implements ActionListener
    private static MyJSQLView_ResourceBundle resourceBundle;
 
    // String for Information About the MyJSQLView.
-   private static String[] myJSQLView_Version = {"MyJSQLView", "3.46", "Build ID: 20130812"};
+   private static String[] myJSQLView_Version = {"MyJSQLView", "3.47", "Build ID: 20131113"};
    private String webSiteString = "http://myjsqlview.org";
    
    private static GeneralProperties generalProperties = new GeneralProperties();
