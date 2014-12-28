64ced695fce2c4f5aaba42645632f6475191fbab
danap@dandymadeproductions.com <danap@dandymadeproductions.com@5ac04320-0ce5-11de-8db4-35e3754be71a>
2014-12-28 00:35:10 +0000

    Added Class Instances. Implemented setRepository() & refresh(). Added Class Methods loadPluginList() & readPluginList().

--- trunk/myjsqlview/src/com/dandymadeproductions/myjsqlview/plugin/FILE_PluginRepository.java
+++ trunk/myjsqlview/src/com/dandymadeproductions/myjsqlview/plugin/FILE_PluginRepository.java
@@ -9,8 +9,8 @@
 //                 << FILE_PluginRepository.java >>
 //
 //=================================================================
-// Copyright (C) 2005-2013 Dana M. Proctor
-// Version 1.2 10/11/2012
+// Copyright (C) 2005-2014 Dana M. Proctor
+// Version 1.3 12/20/2014
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -36,6 +36,8 @@
 //             PluginRepositoryInterface.
 //         1.2 Inserted Place Holder for PluginRepositoryInterface Requirement
 //             refresh();
+//         1.3 Added Class Instances. Implemented setRepository(), & refresh().
+//             Added Class Method loadPluginList() & readPluginList().
 //             
 //-----------------------------------------------------------------
 //                 danap@dandymadeproductions.com
@@ -43,20 +45,39 @@
 
 package com.dandymadeproductions.myjsqlview.plugin;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import com.dandymadeproductions.myjsqlview.utilities.MyJSQLView_Utils;
+
 /**
  *    The FILE_PluginRepository class provides the general framework to
  * create a File type repository that would be derived from a local or
  * networked file system.
  * 
  * @author Dana M. Proctor
- * @version 1.2 10/11/2012
+ * @version 1.3 12/20/2014
  */
 
 public class FILE_PluginRepository extends PluginRepository
 {
    // Class Instances
-   
-   
+   private String remoteRepositoryURL, cachedRepositoryURL;
+   private boolean downloadRepository;
    
    //==============================================================
    // FILE_PluginRepository Constructor
@@ -65,17 +86,109 @@ public class FILE_PluginRepository extends PluginRepository
    public FILE_PluginRepository()
    {
       super();
+      
+      // Just Setup. The downloadRepository used
+      // here for mainly testing, debugging. Since
+      // this type of repository will always try to
+      // download the plugin list if not cached.
+      
+      downloadRepository = true;
+      
+      setType(PluginRepository.FILE);
    }
    
    //==============================================================
-   // Class method to setup up the repository.
-   // The argument should be a reference to a network location.
-   // exp. http://dandymadeproductions.com/
+   // Class method to setup up the repository in this case type
+   // File. The file repository could either be locally or lan.
+   // ex. C:\Users\Documents\plugin
+   // ex. \\pc101\Users\jim\Documents\plugin
+   // ex. /home/user/documents/plugin
    //==============================================================
 
-   public boolean setRepository(String repository)
+   public boolean setRepository(String path)
    {
-      return true;
+      // Method Instances
+      File cachedRepositoryDirectory, cachedRepositoryListFile;
+      String localSystemFileSeparator;
+      boolean validRepository;
+      
+      // Setup
+      
+      if (path.endsWith("/"))
+         remoteRepositoryURL = path + PluginRepository.REPOSITORY_FILENAME;
+      else
+         remoteRepositoryURL = path + "/" + PluginRepository.REPOSITORY_FILENAME;
+      
+      cachedRepositoryDirectoryString = MyJSQLView_Utils.getCacheDirectory() + getName();
+      localSystemFileSeparator = MyJSQLView_Utils.getFileSeparator();
+      validRepository = false;
+      
+      // Check for a valid existing repository that is cached and if
+      // not create at least the directory. A valid repository must
+      // exist, isRepositoryCached, and contain the cached file.
+      
+      cachedRepositoryDirectory = new File(cachedRepositoryDirectoryString);
+      
+      if (cachedRepositoryDirectory.exists() && cachedRepositoryDirectory.isDirectory())
+      {
+         isRepositoryCached = true;
+         
+         cachedRepositoryListFile = new File(cachedRepositoryDirectoryString
+                                             + localSystemFileSeparator
+                                             + PluginRepository.REPOSITORY_CACHED_FILE);
+         
+         if (cachedRepositoryListFile.isFile())
+            validRepository = true;
+         else
+            validRepository = false;
+      }
+      else
+      {
+         isRepositoryCached = cachedRepositoryDirectory.mkdir();
+         validRepository = false;
+      }
+      
+      // Create the cached repository URL String
+      
+      if (isRepositoryCached)
+         cachedRepositoryURL = "file:" + cachedRepositoryDirectoryString
+                               + localSystemFileSeparator
+                               + PluginRepository.REPOSITORY_CACHED_FILE;
+      else
+         cachedRepositoryURL = remoteRepositoryURL;
+      
+      // Determine if a download should occur.
+      
+      if (downloadRepository && isRepositoryCached && !validRepository)
+         validRepository = loadPluginList();
+      
+      // Read the plugin list from the cache & set path to
+      // the cache directory for later identification when
+      // reloading application's plugin frame.
+      
+      if (validRepository)
+         validRepository = readPluginList(true);
+      
+      if (validRepository)
+      {
+         setPath(path);
+         
+         Iterator<Plugin> pluginIterator = getPluginItems().iterator();
+         
+         while (pluginIterator.hasNext())
+         {
+            Plugin currentPlugin = pluginIterator.next();
+            
+            currentPlugin.setPath_FileName(remoteRepositoryURL.substring(0,
+               remoteRepositoryURL.length() - PluginRepository.REPOSITORY_FILENAME.length())
+               + currentPlugin.getJAR());
+            
+            System.out.println("FILE_PluginRepository setRepository() plugin path: "
+                               + currentPlugin.getPath_FileName()); 
+         }
+      }
+      
+      return validRepository;
    }
    
    //==============================================================
@@ -85,6 +198,260 @@ public class FILE_PluginRepository extends PluginRepository
    
    public void refresh()
    {
+      if (isRepositoryCached && loadPluginList())
+         readPluginList(true);
+   }
+   
+   //==============================================================
+   // Class method to load the repository list and in so doing
+   // populate the cache.
+   //==============================================================
+   
+   private boolean loadPluginList()
+   {
+      // Method Instances
+      URL loadURL;
+      URLConnection urlConnection;
       
+      InputStream inputStream;
+      BufferedInputStream bufferedInputStream;
+      FileOutputStream fileOutputStream;
+      BufferedOutputStream bufferedOutputStream;
+      
+      String cacheFileName;
+      byte[] inputBytes;
+      
+      boolean validDownload;
+      
+      // Setup
+      inputStream = null;
+      bufferedInputStream = null;
+      fileOutputStream = null;
+      bufferedOutputStream = null;
+      validDownload = false;
+      
+      try
+      {
+         // System.out.println("FILE_PluginRepository loadPluginList() Downloading Repository List");
+         
+         loadURL = new URL(remoteRepositoryURL);
+         urlConnection = loadURL.openConnection();
+         
+         // Authorization Needed.
+         if(urlConnection.getPermission().getActions().indexOf("read") == -1)
+         {
+            displayErrors("FILE_PluginRepository loadPluginList()\n"
+                          + "Repository URL Requires Read Access.");
+         }
+         // Looks Good.
+         else
+         {
+            cacheFileName = cachedRepositoryURL.replaceFirst("file:", "");
+            
+            // Create Streams
+            inputStream = urlConnection.getInputStream();
+            bufferedInputStream = new BufferedInputStream(inputStream);
+            
+            fileOutputStream = new FileOutputStream(cacheFileName);
+            bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+             
+            // Setup Buffer, read, write, flush.
+            inputBytes = new byte[4096];
+            
+            int n;
+            
+            while ((n = bufferedInputStream.read(inputBytes)) != -1)
+               bufferedOutputStream.write(inputBytes, 0, n);
+            
+            bufferedOutputStream.flush();
+            fileOutputStream.flush();
+            
+            validDownload = true;
+         }
+      }
+      // MalformedURLException, IOException, FileNotFoundException, UnknownHostException?
+      catch(Exception e)
+      {
+         displayErrors("FILE_PluginRepository loadPluginList() Exception:\n" + e.toString());
+      }  
+      finally
+      {
+         try
+         {
+            if (bufferedOutputStream != null)
+               bufferedOutputStream.close();
+         }
+         catch (IOException ioe1)
+         {
+            if (debugMode)
+               System.out.println("FILE_PluginRepository loadloadPluginList() "
+                                  + "Failed to close BufferedOutputStream. " + ioe1.toString());
+         }
+         finally
+         {
+            try
+            {
+               if (fileOutputStream != null)
+                  fileOutputStream.close();
+            }
+            catch (IOException ioe2)
+            {
+               if (debugMode)
+                  System.out.println("FILE_PluginRepository downloadPluginList() "
+                                     + "Failed to close FileOutputStream. " + ioe2.toString());
+            }
+            finally
+            {
+               try
+               {
+                  if (bufferedInputStream != null)
+                     bufferedInputStream.close();
+               }
+               catch (IOException ioe3)
+               {
+                  if (debugMode)
+                     System.out.println("FILE_PluginRepository loadPluginList() "
+                                        + "Failed to close BufferedInputStream. " + ioe3.toString());
+               }
+               finally
+               {
+                  try
+                  {
+                     if (inputStream != null)
+                        inputStream.close();
+                  }
+                  catch (IOException ioe4)
+                  {
+                     if (debugMode)
+                        System.out.println("FILE_PluginRepository loadPluginList() "
+                                           + "Failed to close InputStream. " + ioe4.toString());
+                  }
+               }
+            }
+         }
+      }
+      return validDownload;
+   }
+   
+   //==============================================================
+   // Class method to read the repository plugin list from the
+   // cache.
+   //==============================================================
+   
+   private boolean readPluginList(boolean allowRetry)
+   {
+      // Class methods
+      XMLReader xmlParser;
+      PluginListHandler pluginListHandler;
+      InputStream urlInputStream;
+      InputStream inputStream;
+      InputStreamReader inputStreamReader;
+      InputSource inputSource;
+      
+      boolean validRead;
+      
+      // Setup
+      urlInputStream = null;
+      inputStream = null;
+      inputStreamReader = null;
+      validRead = false;
+      
+      // Try reading & parsing list.
+      try
+      {
+         if (cachedRepositoryURL != remoteRepositoryURL) 
+         {
+            // System.out.println("FILE_PluginRepository readPluginList() Using Cached Plugin List.");
+         }
+         
+         urlInputStream = new URL(cachedRepositoryURL).openStream();
+         
+         xmlParser = XMLReaderFactory.createXMLReader();
+         pluginListHandler = new PluginListHandler(this);
+         inputStream = new BufferedInputStream(urlInputStream);
+         
+         // Check to see if list is xml, or zip/gz file.
+         if(inputStream.markSupported())
+         {
+            inputStream.mark(2);
+            int b1 = inputStream.read();
+            int b2 = inputStream.read();
+            inputStream.reset();
+
+            if(b1 == GZIP_MAGIC_1 && b2 == GZIP_MAGIC_2)
+               inputStream = new GZIPInputStream(inputStream);
+         }
+         
+         // Setup stream and parse.
+         inputStreamReader = new InputStreamReader(inputStream,"UTF8");
+         inputSource = new InputSource(inputStreamReader);
+         
+         inputSource.setSystemId("MyJSQLView.jar");
+         xmlParser.setContentHandler(pluginListHandler);
+         xmlParser.setDTDHandler(pluginListHandler);
+         xmlParser.setEntityResolver(pluginListHandler);
+         xmlParser.setErrorHandler(pluginListHandler);
+         xmlParser.parse(inputSource);
+         
+         validRead = true;
+      }
+      // MalFormedURLException, SAXException, IOException
+      catch (Exception e)
+      { 
+         if (cachedRepositoryURL.startsWith("file:") && allowRetry)
+         {
+            clearPluginItems();
+            
+            if (e instanceof SAXException)
+               loadPluginList();
+            
+            readPluginList(false);
+         }
+         else
+            displayErrors("File_PluginRepository readPluginList() Exception: " + e.toString());
+      }
+      finally
+      {
+         try
+         {
+            if (urlInputStream != null)
+               urlInputStream.close();
+         }
+         catch (IOException ioe)
+         {
+            if (debugMode)
+               System.out.println("FILE_PluginRepository readPluginList() "
+                                  + "Failed to close urlInputStream. " + ioe.toString());
+         }
+         finally
+         {
+            try
+            {
+               if (inputStream != null)
+                  inputStream.close();  
+            }
+            catch (IOException ioe1)
+            {
+               if (debugMode)
+                  System.out.println("FILE_PluginRepository readPluginList() "
+                                     + "Failed to close inputStream. " + ioe1.toString());
+            }
+            finally
+            {
+               try
+               {
+                  if (inputStreamReader != null)
+                     inputStreamReader.close();  
+               }
+               catch (IOException ioe1)
+               {
+                  if (debugMode)
+                     System.out.println("FILE_PluginRepository readPluginList() "
+                                        + "Failed to close inputStreamReader. " + ioe1.toString());
+               }
+            }
+         }
+      }
+      return validRead;
    }
 }
