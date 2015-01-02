ce25730bb86839574af99d79ef2ea981c8d92bd2
danap <danap@dandymadeproductions.com>
2015-01-02 18:16:07 +0000

    Update of Copyright Information.

--- myjsqlview/src/com/dandymadeproductions/myjsqlview/structures/DataExportProperties.java
+++ myjsqlview/src/com/dandymadeproductions/myjsqlview/structures/DataExportProperties.java
@@ -7,7 +7,7 @@
 //           << DataExportProperties.java >>
 //
 //=================================================================
-// Copyright (C) 2005-2013 Dana M. Proctor
+// Copyright (C) 2005-2015 Dana M. Proctor
 // Version 4.6 07/04/2013
 //
 // This program is free software; you can redistribute it and/or
