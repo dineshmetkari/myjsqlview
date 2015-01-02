6d73a05519ca5d800ef7a62d249f346516d8b38b
danap@dandymadeproductions.com <danap@dandymadeproductions.com@5ac04320-0ce5-11de-8db4-35e3754be71a>
2015-01-02 18:19:40 +0000

    Update of Copyright Information.

--- trunk/myjsqlview/src/com/dandymadeproductions/myjsqlview/gui/forms/TableViewForm.java
+++ trunk/myjsqlview/src/com/dandymadeproductions/myjsqlview/gui/forms/TableViewForm.java
@@ -9,7 +9,7 @@
 //                 << TableViewForm.java >>
 //
 //=================================================================
-// Copyright (C) 2005-2014 Dana M. Proctor
+// Copyright (C) 2005-2015 Dana M. Proctor
 // Version 7.1 01/26/2014
 //
 // This program is free software; you can redistribute it and/or
